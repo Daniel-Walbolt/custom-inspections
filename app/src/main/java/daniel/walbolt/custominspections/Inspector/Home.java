a036e285a696ed3ed7538348c6a9b05ae3e29697
Daniel Walbolt <dwalbolt@gmail.com>
2021-10-10 23:22:23 -0400

    Added comments on Home class

--- app/src/main/java/daniel/walbolt/custominspections/Inspector/Home.java
+++ app/src/main/java/daniel/walbolt/custominspections/Inspector/Home.java
@@ -7,6 +7,22 @@ import daniel.walbolt.custominspections.Inspector.Objects.Schedule;
 public class Home
 {
 
+    //The home page is the first page that is loaded when starting the app.
+
+    /*
+
+    Home Page has the following features:
+
+    Schedule an inspection using the Scheduler
+    View scheduled inspections
+    Start a scheduled inspection
+    Remove a scheduled inspection
+    View saved inspections
+
+    Edit the overall theme of the app
+
+     */
+
     private ArrayList<Schedule> schedules;
 
 }
