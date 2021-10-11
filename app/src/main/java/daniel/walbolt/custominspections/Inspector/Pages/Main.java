dcaa227e177611201676653c1622a670f6d6846d
Daniel Walbolt <dwalbolt@gmail.com>
2021-10-11 00:15:22 -0400

    Created several directories and Main Page class.

---
+++ app/src/main/java/daniel/walbolt/custominspections/Inspector/Pages/Main.java
@@ -0,0 +1,26 @@
+package daniel.walbolt.custominspections.Inspector.Pages;
+
+public class Main {
+
+    /*
+
+    The Main page is the first page loaded when viewing/starting an inspection.
+    This page contains:
+
+    The inspection systems and their statuses
+    Generate PDF option
+    Upload data to database option
+    View client information option
+    Add system option
+    Delete system option
+
+     */
+
+    public void Main()
+    {
+
+
+        
+    }
+
+}
